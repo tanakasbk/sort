a8e56b26e5ced1b0a8ebb9048931b6ea3d502293
tanakasbk <tanaka.s.bk@m.titech.ac.jp>
2019-06-25 16:45:58 +0900

    editing

--- src/quick_select_improved.c
+++ src/quick_select_improved.c
@@ -18,35 +18,44 @@ A[0], A[1], ..., A[n-1] の中でk+1番目に小さい値を返す関数
 ただし、Aの中身は書き換えてしまう。
 */
 int quick_select(int A[], int n, int k){
-  int i, j, pivot;
-
+  int i, j, pivot, equal;
 // 真ん中の要素をピボットとする
   pivot = A[n/2];
   A[n/2] = A[0];
   A[0] = pivot;
+//    printf("%dから%d番目に小さい数を探す\n",pivot,k+1);//debug
   for(i = j = 1; i < n; i++){
-    if(A[i] <= pivot){
+    if(A[i] < pivot){
       swap(A+i, A+j);
       j++;
+    }else if(A[i] == pivot){
+        equal++;
     }
   }
-
   if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+  else if(j > k+1) return quick_select(A+1, j-1, k);
+  else if(j+equal > k+1){
+      printf("CHECK\n");
+      return pivot;
+  }
+  else return quick_select(A+j, n-j, k-j);
 }
 
 
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 17; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-
-// すべての要素が同じ場合でも計算が早く終わるか確認する
-
+    int i;
+    
+     A[0] = 0;
+     A[1] = 17; //原始元
+     for(i=2;i<N;i++){
+     A[i] = (long long int) A[i-1] * A[1] % N;
+     }
+     
+    // すべての要素が同じ場合でも計算が早く終わるか確認する
+/*    for(i=0;i<N;i++){
+        A[i] = 17;
+    }
+ */
   for(i=0;i<N;i++){
     if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
 //    printf("%d th element is %d\n", i, quick_select(A, N, i));
